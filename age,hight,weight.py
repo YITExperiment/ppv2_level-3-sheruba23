63cdd09049ae5e96b820b9f040b77308032643e7
sheruba23 <134763424+sheruba23@users.noreply.github.com>
2023-06-25 11:32:38 +0530

    Update and rename age.py to age,hight,weight.py

---
+++ age,hight,weight.py
@@ -0,0 +1,14 @@
+print("=================== Print My Age,Weight,Hight=====================")
+
+age=13
+print(age)
+
+weight = 40
+unit = "kg"
+print(str(weight) + unit)
+
+hight = 100
+unit = "cm"
+print(str(hight) + unit)
+
+
